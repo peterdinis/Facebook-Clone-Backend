135734cdb1cfc36336374fbb4b85625444725c4e
Peterdinis <peterdinis0@pobox.sk>
2022-06-18 15:45:28 +0200

    Creating basic server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import express, {Application} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app: Application = express();
 
-app.listen(5000, () => {
-    console.log("Server Running");
+app.use(cors())
+app.use(express.json());
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI as string).then(() => {
+    console.log("Connected")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server Running on port 5000");
 })
